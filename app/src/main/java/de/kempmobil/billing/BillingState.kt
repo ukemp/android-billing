94ff350f47b9c44334980bf3a1bf156ea8b92efe
Ulrich Kemp <ulrich@kemp.in>
2021-10-01 17:16:17 +0200

    Made BillingState serializable by/added an Int value

--- app/src/main/java/de/kempmobil/billing/BillingState.kt
+++ app/src/main/java/de/kempmobil/billing/BillingState.kt
@@ -1,7 +1,20 @@
 package de.kempmobil.billing
 
-enum class BillingState {
+enum class BillingState(val value: Int) {
 
-    AD_VERSION,
-    FULL_VERSION
+    AD_VERSION(0),
+    FULL_VERSION(1);
+
+
+    companion object {
+
+        fun from(id: Int): BillingState {
+            values().forEach { appState ->
+                if (appState.value == id) {
+                    return appState
+                }
+            }
+            return AD_VERSION
+        }
+    }
 }
